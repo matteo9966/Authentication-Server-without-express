88ea7c17bcb8613a3b92d0b8d17fc59f47389f52
matteo9966 <matteoluigitommasi@gmail.com>
2023-02-18 17:45:46 +0100

    changed log middleware

--- Pipeline/core/middleware/log.middleware.ts
+++ Pipeline/core/middleware/log.middleware.ts
@@ -1,42 +1,51 @@
 import { Middleware } from "../Middleware.types";
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
 import moment from "moment";
-import _ from 'lodash';
-export function logMiddlewareFactory(fsWriteStream:fs.WriteStream):Middleware{
-    
-     //keep a file open
-      
-     
-     const logMiddleware:Middleware = async(request,response)=>{
-              try {
-                const url = request.url
-                const cookies = request.cookies;
-                const headers = request.headers
-                const date = moment().toString();
-                const log = `[${date}]\n[${url}]\nCookies:\n[${cookies||''}]\nHeaders:\n[${(_.toPairs(headers).join('\r\n'))}]\n|--------------END------------|\n`
-                 fsWriteStream.write(log,(err)=>{
-                    if(err){
-                        console.log('errore-write-stream:',err)
-                    }
-                })
+import _ from "lodash";
+export function logMiddlewareFactory(
+  fsWriteStream: fs.WriteStream
+): Middleware {
+  //keep a file open
 
-              } catch (error) {
-                  console.log('[logMiddleware error]:',error)
-              }    
-    }
+  const logMiddleware: Middleware = async (request, response) => {
+      let resp = ""; 
+    request.on("data", (data) => {
+     resp =  resp + data.toString();
+    });
 
-    return logMiddleware
-    
+    request.on("end", () => {
+      try {
+        const url = request.url;
+        const cookies = request.cookies;
+        const headers = request.headers;
+        const date = moment().toString();
+
+        const log = `[${date}]\n[${url}]\nCookies:\n[${
+          cookies || ""
+        }]\nHeaders:\n[${_.toPairs(headers).join("\r\n")}]\n\nBODY \n
+          ${resp}\n
+                    
+                    |--------------END------------|\n`;
+        fsWriteStream.write(log, (err) => {
+          if (err) {
+            console.log("errore-write-stream:", err);
+          }
+        });
+      } catch (error) {
+        console.log("[logMiddleware error]:", error);
+      }
+    });
+  };
+
+  return logMiddleware;
 }
 
-export const writeSteamFactory = (file:string)=>{
-    try {
-        const logStream = fs.createWriteStream(file,{flags:'a'})
-        return logStream
-        
-    } catch (error) {
-        return null
-    }
-  
-}
+export const writeSteamFactory = (file: string) => {
+  try {
+    const logStream = fs.createWriteStream(file, { flags: "a" });
+    return logStream;
+  } catch (error) {
+    return null;
+  }
+};
